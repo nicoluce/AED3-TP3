0dc56e5acb16c597e1e9a0512e02fe0626c6bb66
Nicolas Walter <nicowalter25@gmail.com>
2016-11-02 01:43:39 -0300

    creo que anda lo que me pidio lucas para el 4

---
+++ Ejercicio4/grafo.cpp
@@ -0,0 +1,192 @@
+#include <math.h>
+#include <utility>
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h> 
+#include <algorithm>
+#include <random>
+#include "grafo.h"
+
+#define INF 99999999
+
+
+Grafo::Grafo(unsigned int cantidad_gimnasios, unsigned int cantidad_pokeparadas) {
+	// .reserve() sirve para agrandar el espacio del vector, asi evitamos problemas de resize.
+	this->_pokeparadas.reserve(cantidad_pokeparadas);
+	this->_gimnasios.reserve(cantidad_gimnasios);
+}
+
+unsigned int Grafo::agregar_pokeparada(const Posicion& pos) {
+	// los IDs de los nodos son su posicion en el vector
+	unsigned int new_id = this->_pokeparadas.size() + this->_gimnasios.size() + 1;
+	this->_pokeparadas.push_back(Grafo::Nodo(new_id, pos, Pokeparada));
+	return new_id;
+}
+
+unsigned int Grafo::agregar_gimnasio(const Posicion& pos, unsigned int pociones) {
+	// los IDs de los nodos son su posicion en el vector
+	unsigned int new_id = this->_gimnasios.size() + this->_pokeparadas.size() + 1;
+	this->_gimnasios.push_back(Grafo::NodoGimnasio(new_id, pos, Gimnasio, pociones));
+	return new_id;
+}
+
+const Distancia Grafo::distancia(const Posicion& p1, const Posicion& p2) {
+	return (double) sqrt( (p1.first - p2.first) * (p1.first - p2.first) + (p1.second - p2.second) * (p1.second - p2.second) ); // no se si hace falta el casteo 
+}
+
+// Donde va la magia
+Solucion Grafo::tsp_goloso(unsigned int opcion_primer_nodo, unsigned int capacidad_mochila, unsigned int cantidad_cercanos) {
+	priority_queue<NodoGimnasio, vector<NodoGimnasio>, greater<NodoGimnasio> > heapnasios (_gimnasios.begin(), _gimnasios.end());
+	vector<bool> visitados(_pokeparadas.size(), false);
+
+	unsigned int primer_nodo_indice = elegirPrimerNodo(opcion_primer_nodo);
+	unsigned int pociones_en_mochila = 0;
+	Solucion res;
+
+	visitados[primer_nodo_indice] = true;
+	Posicion posicion_actual = _pokeparadas[primer_nodo_indice].pos;
+
+	if (capacidad_mochila >= 3) pociones_en_mochila += 3;
+	else pociones_en_mochila = capacidad_mochila;
+
+	res.ids.push_back(_pokeparadas[primer_nodo_indice].id);
+	//res.ids.push_back(primer_nodo_indice);
+	while(heapnasios.size() > 0) {
+		while(heapnasios.size() > 0 && pociones_en_mochila >= heapnasios.top().pociones_necesarias) {
+
+			vector<NodoGimnasio> gyms;
+			unsigned int hasta;
+			if(heapnasios.size() >= cantidad_cercanos){
+				hasta = cantidad_cercanos;
+			}else{
+				hasta = heapnasios.size();
+			}	
+			for (unsigned int i = 0; i < hasta; ++i){
+				gyms.push_back(heapnasios.top());
+				heapnasios.pop();
+			}
+			// int indice_a_devolver = gyms.size() - 1;
+			// while(gyms[indice_a_devolver].pociones_necesarias > pociones_en_mochila){
+			// 	indice_a_devolver--;
+			// }
+
+			int indice_a_devolver = gyms.size() - 1;
+			while(gyms[indice_a_devolver].pociones_necesarias > pociones_en_mochila){
+				indice_a_devolver = rand() % gyms.size();
+			}
+
+			NodoGimnasio gym = gyms[indice_a_devolver];
+			res.ids.push_back(gym.id);
+			res.distancia_recorrida += distancia(gym.pos, posicion_actual);
+			posicion_actual = gym.pos;
+
+			pociones_en_mochila -= gym.pociones_necesarias;
+
+			for (int i = 0; i < gyms.size(); ++i){
+				if(i != indice_a_devolver){
+					heapnasios.push(gyms[i]);
+				}
+				
+			}
+
+			
+		}
+
+		int indice_pokeparada_mas_cercana = buscarPociones(pociones_en_mochila,posicion_actual, visitados);
+
+		if (indice_pokeparada_mas_cercana == -1) {
+			res.ids.clear();
+			return res;
+		}
+
+		res.distancia_recorrida += distancia(_pokeparadas[indice_pokeparada_mas_cercana].pos, posicion_actual);
+		posicion_actual = _pokeparadas[indice_pokeparada_mas_cercana].pos;
+		
+		visitados[indice_pokeparada_mas_cercana] = true;
+		
+		if (pociones_en_mochila + 3 <= capacidad_mochila) pociones_en_mochila += 3;
+		else pociones_en_mochila = capacidad_mochila;
+
+		res.ids.push_back(_pokeparadas[indice_pokeparada_mas_cercana].id);
+	}
+
+	return res;
+}
+
+int Grafo::buscarPociones(int mochila, Posicion desde, vector<bool>& visitados){
+	Distancia min = INF;
+
+	int res = -1;
+	unsigned int i = 0;
+	while (i < this->_pokeparadas.size()){
+		if(!(visitados[i]) && distancia(this->_pokeparadas[i].pos, desde) < min){
+			min = distancia(this->_pokeparadas[i].pos, desde);
+			res = i;
+		}
+		i++;
+	}
+	return res;
+}
+
+
+unsigned int Grafo::elegirPrimerNodo(unsigned int opcion){
+	unsigned int res;
+	switch (opcion) {
+		case 1:
+		srand (time(NULL));
+		res = (unsigned int) rand() % _pokeparadas.size();// + _pokeparadas.size();
+		break;
+
+		case 2:
+		unsigned int min = INF;
+		NodoGimnasio gym = _gimnasios[0];
+		for (unsigned int i = 0; i < _gimnasios.size(); ++i){
+			if(_gimnasios[i].pociones_necesarias < min ){
+				gym = _gimnasios[i];
+				min = _gimnasios[i].pociones_necesarias;
+			}
+		}
+
+		unsigned int pociones = 0;
+		vector<unsigned int> paradas;
+		double mini = INF;
+		Posicion desde = gym.pos;
+		Nodo parada;
+		while(pociones < gym.pociones_necesarias){
+			mini = INF;
+			for (unsigned int i = 0; i < _pokeparadas.size(); ++i){
+				if(distancia(_pokeparadas[i].pos, desde) < mini && desde!= _pokeparadas[i].pos){
+					mini = distancia(_pokeparadas[i].pos, desde);
+					parada = _pokeparadas[i];
+					res = i;
+				}
+			desde = parada.pos;
+			}
+			pociones +=3;
+		}
+
+		break;
+
+	}
+
+	return res;
+
+}
+
+// para imprimir el grafo, puede ser util
+const void Grafo::imprimir() {
+	cout << "{ \n";
+	for (unsigned int i = 0; i < this->_gimnasios.size(); i++) {
+		cout << "	" << this->_gimnasios[i].id;
+		cout << " G " << this->_gimnasios[i].pociones_necesarias << ' ';
+
+		cout << "<" << this->_gimnasios[i].pos.first << ", " << this->_gimnasios[i].pos.second << ">\n";
+	}
+
+	for (unsigned int i = 0; i < this->_pokeparadas.size(); i++) {
+		cout << "	" << this->_pokeparadas[i].id;
+		cout << " P ";
+
+		cout << "<" << this->_pokeparadas[i].pos.first << ", " << this->_pokeparadas[i].pos.second << ">\n";
+	}
+	cout << "}" << endl;
+}
---
+++ Ejercicio4/main.cpp
@@ -0,0 +1,62 @@
+// TP3 - Algoritmo y Estructuras de Datos III
+// Algoritmo Exacto / Backtracking
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+#include "grafo.h"
+#include "../Auxiliares/auxiliares.h"
+
+using namespace std;
+
+void parseo_entrada(Grafo& g, int n, int m) {
+	string buffer;
+	vector<int>entrada_parseada;
+
+	for (int i = 0; i < n; i++) {
+		getline(cin, buffer, '\n');
+		entrada_parseada = split(buffer, ' ');
+		g.agregar_gimnasio(pair<int, int>(entrada_parseada[0], entrada_parseada[1]), entrada_parseada[2]);
+	}
+
+	for (int i = 0; i < m; i++) {
+		getline(cin, buffer, '\n');
+		entrada_parseada = split(buffer, ' ');
+		g.agregar_pokeparada(pair<int, int>(entrada_parseada[0], entrada_parseada[1]));
+	}
+}
+
+int main(int argc, char const *argv[]) {
+	
+	string primera_linea;
+	getline(cin, primera_linea, '\n');
+
+	vector<int>entrada_parseada (split(primera_linea, ' '));
+
+	int cantidad_gimnasios = entrada_parseada[0];
+	int cantidad_paradas = entrada_parseada[1];
+	int capacidad_mochila = entrada_parseada[2];
+
+	// cout << capacidad_mochila << endl;
+	
+	Grafo g(cantidad_gimnasios, cantidad_paradas);
+	parseo_entrada(g, cantidad_gimnasios, cantidad_paradas);
+
+	//for (int i = 0; i < cantidad_paradas; i++) {
+			
+		Solucion res = g.tsp_goloso(2, capacidad_mochila, 4); // el primer parametro define como se elige el primer nodo,
+		//el tercero es la cantidad de gimnasios que toma del heap al hacer el random
+		srand(time(NULL));
+		if (res.ids.empty()) cout << -1 << endl;
+		else {
+			cout << res.distancia_recorrida << ' ' <<  res.ids.size() - 1 << ' ';
+			imprimir_vector(res.ids);
+		}
+	//}
+
+
+	//g.imprimir();
+
+	return 0;
+}
